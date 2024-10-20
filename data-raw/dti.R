5771d38b454e6db466329f451aad7db31e5ef206
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-10-20 22:25:53 +0200

    feat: add initial dti dataset

---
+++ data-raw/dti.R
@@ -0,0 +1,10 @@
+dti <- tibble::tibble(
+  id = refund::DTI$ID,
+  visit = refund::DTI$visit,
+  sex = refund::DTI$sex,
+  case = factor(ifelse(refund::DTI$case, "MS", "control")),
+  cca = tf::tfd(refund::DTI$cca, arg = seq(0, 1, length.out = 93)),
+  rcst = tf::tfd(refund::DTI$rcst, arg = seq(0, 1, length.out = 55))
+)
+
+usethis::use_data(dti, overwrite = TRUE)
