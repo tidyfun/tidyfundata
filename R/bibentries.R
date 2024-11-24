7a05495fa754a591980faa400ed3893469366e5d
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-11-24 17:25:25 +0100

    feat: add citations for running dataset and restructure files

---
+++ R/bibentries.R
@@ -0,0 +1,35 @@
+# nolint start
+bibentries <- c(
+  liew2016a = bibentry("article",
+    title = "Effects of two neuromuscular training programs on running biomechanics with load carriage: a study protocol for a randomised controlled trial",
+    author = "Liew, B. X. and Morris, S. and Keogh, J. W. and Appleby, B. and Netto, K.",
+    journal = "BMC Musculoskeletal Disorders",
+    volume = "17",
+    number = "1",
+    pages = "1-10",
+    year = "2016",
+    key = "2016a",
+    publisher = "BioMed Central"
+  ),
+  liew2016b = bibentry("article",
+    title = "The effects of load carriage on joint work at different running velocities",
+    author = "Liew, B. X. and Morris, S. and Netto, K.",
+    journal = "Journal of Biomechanics",
+    volume = "49",
+    number = "14",
+    pages = "3275-3280",
+    year = "2016",
+    key = "2016b",
+    publisher = "Elsevier"
+  ),
+  fukuchi2017public = bibentry("article",
+    title = "A public dataset of running biomechanics and the effects of running speed on lower extremity kinematics and kinetics",
+    author = "Fukuchi, Reginaldo K and Fukuchi, Claudiane A and Duarte, Marcos",
+    journal = "PeerJ",
+    volume = "5",
+    pages = "e3298",
+    year = "2017",
+    publisher = "PeerJ Inc."
+  )
+)
+# nolint end
--- R/running.R
+++ R/running.R
@@ -1,35 +1,3 @@
-#' @importFrom tibble tibble
-NULL
-
-#' Diffusion tensor imaging data
-#'
-#' @description
-#' Fractional anisotropy (FA) tract profiles for the corpus
-#' callosum (cca) and the right corticospinal tract (rcst). Accompanying
-#' the tract profiles are the subject ID numbers, visit number, total
-#' number of scans, multiple sclerosis case status and Paced Auditory Serial
-#' Addition Test (pasat) score.
-#'
-#' Data are also include in the `refund` package in another format.
-#'
-#' @details
-#' If you use this data as an example in written work, please
-#' include the following acknowledgment: “The MRI/DTI data were collected
-#' at Johns Hopkins University and the Kennedy-Krieger Institute"
-#'
-#' @format A tibble with 382 rows and 6 variables:
-#' \describe{
-#'   \item{id}{Subject ID}
-#'   \item{visit}{The visit number}
-#'   \item{sex}{The sex of the subject}
-#'   \item{case}{The multiple sclerosis case status}
-#'   \item{cca}{The fractional anisotropy (FA) tract profile for the corpus callosum}
-#'   \item{rcst}{The fractional anisotropy (FA) tract profile for the right corticospinal tract}
-#' }
-#' @examples
-#' dti
-"dti"
-
 #' Running data
 #'
 #' @description
@@ -59,6 +27,7 @@ NULL
 #' }
 #'
 #' @source <https://github.com/davidruegamer/FDA_tutorial/blob/main/data/data_comb.RDS>
+#' @references `r format_bib("liew2016a", "liew2016b", "fukuchi2017public")`
 #' @format A tibble with 610 rows and 49 variables:
 #' \describe{
 #'   \item{id}{subject identifier}
