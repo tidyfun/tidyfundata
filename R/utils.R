2c6730f806f6e3e0938dee5916408ba0cf528cdf
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-11-26 20:22:32 +0100

    chore: remove unused `cite_bib()` function

--- R/utils.R
+++ R/utils.R
@@ -10,25 +10,3 @@ format_bib <- function(..., bibentries = NULL, envir = parent.frame()) {
   )
   paste0(str, collapse = "\n\n")
 }
-
-cite_bib <- function(..., bibentries = NULL, envir = parent.frame()) {
-  if (is.null(bibentries)) {
-    bibentries <- get("bibentries", envir = envir)
-  }
-  stopifnot(is.list(bibentries), anyDuplicated(names(bibentries)) == 0L)
-
-  str <- vapply(
-    list(...),
-    function(entry) {
-      x <- bibentries[[entry]]
-      sprintf("%s (%s)", x$author[[1L]]$family %||% x$author[[1L]], x$year)
-    },
-    NA_character_
-  )
-
-  if (length(str) >= 3L) {
-    str <- c(toString(utils::head(str, -1L)), utils::tail(str, 1L))
-  }
-
-  paste0(str, collapse = " and ")
-}
